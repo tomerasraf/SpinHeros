a9f4eea1909ef9b8ccfbddb3770bc734fc79f06d
TomerAsraf <tomerasraf2@gmail.com>
2022-03-09 21:54:36 +0200

    v0.0.1
    * Scriptable object game event system
    
    * Event Listeners

---
+++ Assets/Scripts/GameEvents/Listeners/BaseGameEventListener.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class BaseGameEventListener<T, E, UER> : MonoBehaviour, IGameEventListener<T> where E : BaseGameEvent<T> where UER : UnityEvent<T>
+{
+    [SerializeField] private E gameEvent;
+    public E GameEvent { get { return gameEvent; } set { gameEvent = value; } }
+
+    [SerializeField] private UER unityEventResponse;
+
+    private void OnEnable()
+    {
+        if (gameEvent == null) { return; }
+        gameEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        if (gameEvent == null) { return; }
+        {
+            GameEvent.UnregiterListener(this);
+        }
+    }
+
+    public void OnEventRaised(T item)
+    {
+        if (unityEventResponse != null)
+        {
+            unityEventResponse.Invoke(item);
+        }
+    }
+}
