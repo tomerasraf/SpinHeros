7f870e7da1fe3b9caefe6b2260750a7c7e683b0d
Tomer Asraf <tomerasraf2@gmail.com>
2022-03-10 12:54:12 +0200

    v0.0.1
    - Spin Logic

--- Assets/Scripts/Logic/SpinLogic.cs
+++ Assets/Scripts/Logic/SpinLogic.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class SpinLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int[] slot1 = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5 };
+    private int[] slot2 = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5 };
+    private int[] slot3 = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5 };
+    private int slot1Result;
+    private int slot2Result;
+    private int slot3Result;
+
+    [SerializeField] float timeOfSpin = 1f;
+
+    [SerializeField] private bool isButtonPressed = false;
+
+    public void PressButton()
     {
-        
+        isButtonPressed = true;
+
+        RanomizeSpin(slot1, slot1Result);
+        RanomizeSpin(slot2, slot2Result);
+        RanomizeSpin(slot3, slot3Result);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isButtonPressed)
+        {
+            timeOfSpin -= Time.deltaTime;
+            if (timeOfSpin <= 0)
+            {
+                isButtonPressed = false;
+                timeOfSpin = 1f;
+            }
+        }
+    }
+
+    private void RanomizeSpin(int[] slot, int result)
+    {
+        int rand = Random.Range(0, slot.Length);
+        result = slot[rand];
     }
 }
