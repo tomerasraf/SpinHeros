c253bf8c36f184fb57720695a59a35294470b2e3
Arian Studio <michael.pozd@gmail.com>
2022-04-07 00:01:44 +0300

    Revert "V0.0.8"
    This reverts commit 6bbd364c4943ea2410743ac8234ae997a6446a69.

--- Assets/Scripts/Simulation/Fish.cs
+++ Assets/Scripts/Simulation/Fish.cs
@@ -5,13 +5,6 @@ public class Fish : MonoBehaviour
     [SerializeField] Animator[] players;
     [SerializeField] GameObject fishPrefab;
 
-    public void CatchFish_Anim(int playerID)
-    {
-        players[playerID].SetBool("isSpining", true);
-    }
+    
 
-    public void Idel_Anim(int playerID)
-    {
-        players[playerID].SetBool("isSpining", false);
-    }
 }
