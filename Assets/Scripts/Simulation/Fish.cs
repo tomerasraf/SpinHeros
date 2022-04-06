6bbd364c4943ea2410743ac8234ae997a6446a69
TomerAsraf <tomerasraf2@gmail.com>
2022-04-06 23:22:37 +0300

    V0.0.8
    + fish anim

--- Assets/Scripts/Simulation/Fish.cs
+++ Assets/Scripts/Simulation/Fish.cs
@@ -5,6 +5,13 @@ public class Fish : MonoBehaviour
     [SerializeField] Animator[] players;
     [SerializeField] GameObject fishPrefab;
 
-    
+    public void CatchFish_Anim(int playerID)
+    {
+        players[playerID].SetBool("isSpining", true);
+    }
 
+    public void Idel_Anim(int playerID)
+    {
+        players[playerID].SetBool("isSpining", false);
+    }
 }
