3fbc68789689fd13808bb49846653fcf230438a4
TomerAsraf <tomerasraf2@gmail.com>
2022-04-07 00:03:21 +0300

    V0.0.8
    + anim fix

--- Assets/Scripts/Simulation/Fish.cs
+++ Assets/Scripts/Simulation/Fish.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Fish : MonoBehaviour
 {
     [SerializeField] Animator[] players;
     [SerializeField] GameObject fishPrefab;
 
-    public void CatchFish_Anim(int playerID)
+    private float animDuration = 1.03f;
+
+    public void startCatchFish_Coroutine(int playerID)
     {
-        players[playerID].SetBool("isSpining", true);
+        StartCoroutine(CatchFish_Anim(playerID));
     }
 
-    public void Idel_Anim(int playerID)
+    IEnumerator CatchFish_Anim(int playerID)
     {
+        players[playerID].SetBool("isSpining", true);
+        yield return new WaitForSeconds(animDuration);
         players[playerID].SetBool("isSpining", false);
+        yield return null;
     }
+
 }
