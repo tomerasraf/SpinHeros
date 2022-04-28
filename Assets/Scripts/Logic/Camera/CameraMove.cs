d9a2f1c6c28faa4019c40ef894bfb44278f6e35a
TomerAsraf <tomerasraf2@gmail.com>
2022-04-28 23:03:51 +0300

    V0.0.9
    + Camera movement

---
+++ Assets/Scripts/Logic/Camera/CameraMove.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    //1. Detect Player touch the screen.
+    //2. Store this postion as the origin.
+    //3. Store the current position of the touch.
+    //4. calculate the diffrence bettwen the origin to the curent postion of the touch.
+    //5. move the position of the camera to diffrence.
+
+    [SerializeField] float cameraSensitivity;
+    private Vector3 startTouchPos;
+    private Vector3 currentTouchPos;
+    private Vector3 difference;
+
+    private void LateUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch finger = Input.GetTouch(0);
+            if (finger.phase == TouchPhase.Began)
+            {
+                startTouchPos = finger.position;
+            }
+
+            if (finger.phase == TouchPhase.Moved)
+            {
+                currentTouchPos = finger.position;
+                difference = startTouchPos - currentTouchPos;
+                Camera.main.transform.position = difference * cameraSensitivity * Time.fixedDeltaTime;
+            }
+
+            if (finger.phase == TouchPhase.Ended || finger.phase == TouchPhase.Canceled)
+            {
+                startTouchPos = Camera.main.transform.position;
+            }
+        }
+    }
+}
