e35577b7bbbf3982f77b1bfc1e6eec706716bb42
TomerAsraf <tomerasraf2@gmail.com>
2022-05-10 23:04:57 +0300

    V0.0.9
    + Smooth UI Updater

--- Assets/Scripts/UI/UI/Slot Machine/CoinsUI.cs
+++ Assets/Scripts/UI/UI/Slot Machine/CoinsUI.cs
@@ -1,13 +1,49 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
+using DG.Tweening;
 
 public class CoinsUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI coinText;
     [SerializeField] PlayerData _playerData;
+    [SerializeField] float smoothVelocity;
+    [SerializeField] float smoothSpeed;
+    private float displayCoins = 0;
 
-    public void CoinUI_Updater()
+
+    public void GainCoinUI_Updater()
     {
-        coinText.text = _playerData.coins.ToString();
+        StartCoroutine(GainCoinUI_Coroutine());
     }
+
+    public void SpendCoinUI_Updater()
+    {
+
+        StartCoroutine(SpendCoinUI_Coroutine());
+    }
+
+    IEnumerator GainCoinUI_Coroutine()
+    {
+        while (displayCoins < _playerData.coins)
+        {
+            displayCoins = displayCoins + smoothVelocity * smoothSpeed;
+            coinText.text = displayCoins.ToString();
+            yield return null;
+        }
+        yield return null;
+    }
+
+    IEnumerator SpendCoinUI_Coroutine()
+    {
+        while (displayCoins > _playerData.coins)
+        {
+            displayCoins = displayCoins - smoothVelocity * smoothSpeed;
+            coinText.text = displayCoins.ToString();
+            yield return null;
+        }
+        yield return null;
+    }
+
+
 }
