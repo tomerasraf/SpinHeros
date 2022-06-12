e379c505d9c1d1ea10a81a6b2d8e4df388444683
TomerAsraf <tomerasraf2@gmail.com>
2022-06-12 20:52:45 +0300

    V0.1.5
    + Data manager update

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -37,6 +37,7 @@ public class DataManager : MonoBehaviour
                 TutorialPlayerGameData();
             }
 
+            _playerData[0].bet = 1;
             ResetWorldData();
             ResetslotMachineOddsData();
         }
