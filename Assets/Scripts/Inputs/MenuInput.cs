4a17b28a7c7c59149ebee768dea316293d23f2c2
TomerAsraf <tomerasraf2@gmail.com>
2022-06-22 18:56:23 +0300

    V0.1.7
    + Menu UI
    + Menu UI slide animation

---
+++ Assets/Scripts/Inputs/MenuInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+class MenuInput : MonoBehaviour
+{
+    [SerializeField] VoidEvent openMenuUI;
+
+    public void OnClickMenuButton_Listener()
+    {
+        openMenuUI.Raise();
+    }
+
+}
---
+++ Assets/Scripts/UI/UI/Menu/MenuUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+class MenuUI : MonoBehaviour
+{
+    [SerializeField] GameObject menuUI;
+    [SerializeField] GameObject backgroundUI;
+
+    private float endValueMenuOffset = 250;
+
+    public void MenuButton_Listener() {
+        menuUI.SetActive(true);
+
+        menuUI.transform.DOMoveX(endValueMenuOffset, 1f);        
+
+
+        backgroundUI.GetComponent<Image>().DOFade(0, 0).OnComplete(() =>
+        {
+            backgroundUI.SetActive(true);
+            backgroundUI.GetComponent<Image>().DOFade(0.6f, 1);
+        });
+    }
+}
+
+
